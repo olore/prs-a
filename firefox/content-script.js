9134aca4a40689df34487c1865919582464ba444
Brian Olore <brian@olore.net>
2018-06-01 17:07:44 -0400

    use innerHTML, it's easier

--- firefox/content-script.js
+++ firefox/content-script.js
@@ -23,7 +23,7 @@ class ContentScriptHandler {
     let commentTexts = this.getCommentsFromPage(commentNodes);
 
     if (request.start === 1) {
-      // send array of comment strings to background script
+      // return array of comment strings to background script
       return Promise.resolve({response: commentTexts});
     } else {
       // receive array of comment strings & scores from background script
@@ -51,22 +51,18 @@ class ContentScriptHandler {
     return commentTexts;
   }
 
-  // if we are going to use jQuery, maybe just use https://api.jquery.com/html/#html2
-  // or stop using jQuery completely ?
   generateHtmlNode(score) {
-    let wrapperDiv = document.createElement('div');
-    wrapperDiv.setAttribute('class', this.MESSAGE_CLASS);
+    let html = `
+      <div class="${this.MESSAGE_CLASS}">
+        <span style="transform: rotate(${180 * (1 - score)}deg); width: 25px; height: 25px; padding: 2px; float: left">
+          👍
+        </span>
+        ${(score * 100).toFixed(2)}% positive
+      </div>
+    `;
 
-    let messageDiv = document.createElement('div');
-    messageDiv.setAttribute('class', this.MESSAGE_CLASS);
-    messageDiv.innerText = `${(score * 100).toFixed(2)}% positive`;
-
-    let thumbDiv = document.createElement('div');
-    thumbDiv.innerText = '👍';
-    thumbDiv.style = `transform: rotate(${180 * (1 - score)}deg); width: 25px; height: 25px; padding: 2px; float: left`;
-
-    wrapperDiv.appendChild(thumbDiv);
-    wrapperDiv.appendChild(messageDiv);
-    return wrapperDiv;
+    let div = document.createElement('div');
+    div.innerHTML = html;
+    return div;
   }
 }
