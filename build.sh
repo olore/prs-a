3d6b676e54b516cda984a43e77b48385ee075b24
Brian Olore <brian@olore.net>
2018-06-08 07:16:50 -0400

    cleanup build script a bit - use dist/

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
-echo "DON'T FORGET TO BUMP THE manfest.json FILES"
-cd firefox && zip -r prs-a.zip *
+mkdir -p dist
+cd firefox && zip -r firefox.zip * && mv firefox.zip ../dist
 cd -
-cd chrome && zip -r prs-a.zip *
+cd chrome && zip -r chrome.zip * && mv chrome.zip ../dist
 cd -
+echo ""
+echo ""
+echo "  DID YOU BUMP THE manfest.json FILES ???"
+echo ""
+echo ""
